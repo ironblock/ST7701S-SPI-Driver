f07869605fdf5eb6895d30251e64230809b05944
Corey Vixie <corey@vixie.enterprises>
2020-08-02 14:05:33 -0700

    Allow the SPI device to be passed as an argument, in case it's nonstandard

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod spi;
 use panel::TDOMode;
 use sequence_tdo::init;
 use spi::ST7701S;
+use std::env;
 
 /// ST7701S supports two kinds of RGB interface, DE mode (mode 1) and HV mode
 /// (mode 2), and 16bit/18bit and 24 bit data format. When DE mode is selected
@@ -20,7 +21,17 @@ use spi::ST7701S;
 fn main() {
     println!("Initializing SPI driver for ST7701S panel");
 
-    let mut display = ST7701S::new(String::from("/dev/spidev1.0"));
+    let args: Vec<String> = env::args().collect();
+    let spi_argument: Option<String> = Some(String::from(&args[0]));
+    let mut spi_path: String = String::from("/dev/spidev0.0");
+
+    if !spi_argument.is_some() {
+        spi_path = spi_argument.unwrap();
+    }
+
+    println!("Using SPI device path {}", spi_path);
+
+    let mut display = ST7701S::new(spi_path);
     let mode = TDOMode;
 
     init(&mut display, mode);
